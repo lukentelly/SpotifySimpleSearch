13b9209f4de3d1b65849dcbaa7be65b8e3d87e1f
lukentelly <lukentelly@gmail.com>
2015-07-01 21:10:10 -0400

    switched asynchronus call to asynctask from spotifies version

--- app/src/main/java/com/example/aaronluke/spotifysimplesearch/MainActivityFragment.java
+++ app/src/main/java/com/example/aaronluke/spotifysimplesearch/MainActivityFragment.java
@@ -3,27 +3,33 @@ package com.example.aaronluke.spotifysimplesearch;
 // various imported libraries
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.EditText;
 import android.view.View;
 import android.view.ViewGroup;
-import android.content.res.Resources;
+import java.io.IOException;
+import android.widget.Toast;
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO: remove once doen: from old design- keeping around "just in case"
+import android.content.Context;
+import android.content.res.Resources;
 import retrofit.Callback;
 import retrofit.client.Response;
 import retrofit.RetrofitError;
-import android.util.Log;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+// TODO: will need to make fragment for second screen and switch them
+
+// Fragment for Main Screen
 public class MainActivityFragment extends Fragment {
 
     // list of Artists
@@ -42,24 +48,30 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        // TODO: need to work on bundling state
+        super.onCreate(savedInstanceState);
+
         // allocate array list
-        artistList= new ArrayList();
+        artistList = new ArrayList();
 
-        // perform artist search and add to artistStringList
+        // grabbing rootView to manipulate
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
         // TODO: pass in from text box instead of static
-        performArtistSearch("Keith");
 
-        // populatin
-        // g adapter with current activity, layout ID, id of textview, and the string data
+        // get reference to artist input box
+        EditText inputArtistBox = (EditText) rootView.findViewById(R.id.artist_name_input);
+
+        // perform artist search and add to artistStringList in background
+        new performArtistSearch().execute("Sara");
+
+        // populating adapter with current activity, layout ID, id of textview, and the string data
         mArtistAdapter =
                 new ArrayAdapter<>(
                         getActivity(),
                         R.layout.artist_list,
                         R.id.artist_list_text_view,
                         artistList);
-
-        // grabbing rootView to manipulate
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         // retrieving a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.artist_list_view);
@@ -70,44 +82,49 @@ public class MainActivityFragment extends Fragment {
     }
 
     // method to search for Artist
-    private void performArtistSearch (String query) {
+    public class performArtistSearch extends AsyncTask<String, Void, Void> {
 
-        SpotifyApi api = new SpotifyApi();
-        SpotifyService spotify = api.getService();
 
-        // getting pointer to resources- will pull strings, views from this
-        // Resources res = getResources();
+        @Override
+        protected Void doInBackground(String... params) {
 
-        spotify.searchArtists(query, new Callback<ArtistsPager>() {
-            @Override
-            public void success(ArtistsPager artistsPager, Response response) {
+            // TODO: implement try/catch
+            //try {
+            String query = params[0];
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
 
-                // grab all artists that match
-                List<Artist> listOfArtists = artistsPager.artists.items;
+            ArtistsPager artistsPager = spotify.searchArtists(query);
 
-                // cycle through artist list names to add to artistStringList
-                for(Artist element : listOfArtists){
-                    artistList.add(element.name);
-                    Log.d("Name", element.name);
-                }
+            // grab all artists that match
+            List<Artist> listOfArtists = artistsPager.artists.items;
 
-                // notify adapter that data changed via notifyDataSetchange
-
-                Log.d("artist success", artistsPager.toString());
-
+            // cycle through artist list names to add to artistStringList
+            for (Artist element : listOfArtists) {
+                artistList.add(element.name);
+                Log.d("Name", element.name);
             }
 
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d("artist failure", error.toString());
+            // Log success
+            Log.d("artist success", artistsPager.toString());
 
-            }
-        });
+            // Todo: pop up toast to declare success
+            // Toast.makeText(getApplicationContext(), "Artist(s) Found!", Toast.LENGTH_SHORT).show();
+
+            return null;
+
+            //} catch (IOException e) {
+
+            //Log.d("artist failure",);
+            // Todo: pop up toast to declare success
+            //oast.makeText(getView().getContext(), "Artist Not Found", Toast.LENGTH_SHORT).show();
+            //}
+
+        }
+
+        // TODO: need to implement a call back for the text editor
+        // onEditorActionListener(new TextView.OnEditorActionListener()
+
 
     }
-
-    // TODO: need to implement a call back for the text editor
-    // onEditorActionListener(new TextView.OnEditorActionListener()
-
-
 }
